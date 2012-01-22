3e8f1befdb92edc06f7c99814eecf8f8a3b2731c
Glen Langer <contao.modul@glen-langer.de>
2012-01-22 01:00:51 +0100

    change version definitions

--- ModuleVisitorVersion.php
+++ ModuleVisitorVersion.php
@@ -8,7 +8,7 @@
  * Modul Visitors Version Definition
  *
  * PHP version 5
- * @copyright  Glen Langer 2009..2011
+ * @copyright  Glen Langer 2009..2012
  * @author     Glen Langer 
  * @package    GLVisitors 
  * @license    LGPL 
@@ -16,7 +16,7 @@
  */
 
 // require_once(TL_ROOT . '/system/modules/visitors/ModuleVisitorVersion.php');
-define('VISITORS_VERSION', '2.5');
-define('VISITORS_BUILD'  , '4');
+define('VISITORS_VERSION', '2.6');
+define('VISITORS_BUILD'  , '0');
 
 ?>
