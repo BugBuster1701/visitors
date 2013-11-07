0022a6330dfd809850d4483450c339e29153167b
Glen Langer <contao.modul@glen-langer.de>
2013-11-07 23:43:20 +0100

    Version 3.2.1

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@
  */
 
 define('VISITORS_VERSION', '3.2');
-define('VISITORS_BUILD'  , '0');
+define('VISITORS_BUILD'  , '1');
 
 /**
  * -------------------------------------------------------------------------
