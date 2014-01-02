cec6602a3d62b940dfa2b943fa21c4c78020bb29
Glen Langer <contao.modul@glen-langer.de>
2014-01-02 17:45:27 +0100

    Fixed #78, Berechung YearLastWeek

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@
  */
 
 define('VISITORS_VERSION', '3.2');
-define('VISITORS_BUILD'  , '1');
+define('VISITORS_BUILD'  , '2');
 
 /**
  * -------------------------------------------------------------------------
