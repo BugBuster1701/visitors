56b99d05d4c5f229ae128c13c1685d146a76cd1b
Glen Langer <contao.modul@glen-langer.de>
2015-02-10 23:44:07 +0100

    Update Transifex Language Files #106, ru, hi

---
+++ languages/hi/tl_visitors_referrer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-visitors/language/hi/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-02-10T23:35:12+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_visitors_referrer']['number'] = 'संख्या';
+
