798764ae16bed3deae73e169c43391790bd710d6
Glen Langer <contao.modul@glen-langer.de>
2015-05-09 22:34:28 +0200

    Update Transifex Language Files #106, hi

--- languages/hi/modules.php
+++ languages/hi/modules.php
@@ -11,8 +11,10 @@
  * last-updated: 2015-02-10T23:35:05+01:00
  */
 
-
 $GLOBALS['TL_LANG']['FMD']['visitors']['0']    = 'परिदर्शक';
+$GLOBALS['TL_LANG']['FMD']['visitors']['1']    = 'आने वाले और देखें गए की गणना';
 $GLOBALS['TL_LANG']['MOD']['visitors']['0']    = 'परिदर्शक';
+$GLOBALS['TL_LANG']['MOD']['visitors']['1']    = 'यह मापांक आपको आपके जालस्थल पर आने वालों के व संख्या बताता है।';
 $GLOBALS['TL_LANG']['MOD']['visitorstat']['0'] = 'परिदर्शक आंकड़े';
+$GLOBALS['TL_LANG']['MOD']['visitorstat']['1'] = 'आने वालों के मापांक द्वारा आंकड़े';
 
