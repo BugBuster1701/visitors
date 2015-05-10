829f5b9ce93779fc7f51e60b64f72786532d3264
Glen Langer <contao.modul@glen-langer.de>
2015-05-10 18:27:47 +0200

    Fixed #176, [Insight] PHP code should follow PSR-1 basic coding standard

--- config/autoload.php
+++ config/autoload.php
@@ -43,7 +43,7 @@ ClassLoader::addClasses(array
 	// Classes
 	'BugBuster\Visitors\ModuleVisitorBrowser3'          => 'system/modules/visitors/classes/ModuleVisitorBrowser3.php',
 	'BugBuster\Visitors\ModuleVisitorStatScreenCounter' => 'system/modules/visitors/classes/ModuleVisitorStatScreenCounter.php',
-	'BugBuster\Visitors\DCA_module_visitors'            => 'system/modules/visitors/classes/DCA_module_visitors.php',
+	'BugBuster\Visitors\DcaModuleVisitors'              => 'system/modules/visitors/classes/DcaModuleVisitors.php',
 	'BugBuster\Visitors\DCA_visitors'                   => 'system/modules/visitors/classes/DCA_visitors.php',
 	'BugBuster\Visitors\DcaVisitorsCategory'            => 'system/modules/visitors/classes/DcaVisitorsCategory.php',
 	'BugBuster\Visitors\ModuleVisitorStatPageCounter'   => 'system/modules/visitors/classes/ModuleVisitorStatPageCounter.php',
